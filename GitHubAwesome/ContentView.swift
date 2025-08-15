f7d58f06bdcd4a2bbb59255b55ecd6e04920316f
Liam <khoasdyn@gmail.com>
2025-08-16 06:29:48 +0700

    Initial Commit

---
+++ GitHubAwesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubAwesome
+//
+//  Created by khoasdyn on 16/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubAwesome/GitHubAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAwesomeApp.swift
+//  GitHubAwesome
+//
+//  Created by khoasdyn on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
